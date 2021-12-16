370bbe8c9c8df0fcbba43d20528e93b44dfc3677
Mayuri <mayuriwasu2000@gmail.com>
2021-12-17 01:46:35 +0530

    updated sliders

--- Home_Page/slider.js
+++ Home_Page/slider.js
@@ -1,3 +1,7 @@
+
+// master slider
+
+
 let count = 0; 
 let slideshowData = [
     "https://media6.ppl-media.com/tr:w-1280,c-at_max,f-gif/mediafiles/ecomm/misc/1639401029_header-red-banner-web-1.gif",
@@ -58,3 +62,190 @@ let timer = setInterval(function() {
     else if (count == 0)  count = slideshowData.length -1 ;
     slideShow(count);
 },3000);
+
+
+
+// another  master slider
+
+
+
+// cards vala slider
+
+
+function brandSlider() {
+    var slideList = document.querySelector(".slider");
+  
+    var count = 1;
+    var click = 8;
+    var size = document.querySelector(".slider").offsetWidth;
+    if (size < 900) {
+      click = 11;
+    }
+    if (count == 1) {
+      document.getElementById("leftSwipe").style.opacity = 0.5;
+      document.getElementById("leftSwipe").style.display = "hidden";
+    }
+    console.log(size);
+    function stepBack() {
+      if (count == 2) {
+        document.getElementById("leftSwipe").style.opacity = 0.5;
+      }
+      if (count > 1) {
+        count = count - 2;
+        slideList.style.left = "-" + count * 31 + "rem";
+  
+        document.getElementById("rightSwipe").style.opacity = 1;
+  
+        count++;
+      } else {
+        document.getElementById("leftSwipe").style.opacity = 0.5;
+        document.getElementById("leftSwipe").style.display = "hidden";
+      }
+    }
+    function stepForward() {
+      if (count == click - 1) {
+        document.getElementById("rightSwipe").style.opacity = 0.5;
+        document.getElementById("rightSwipe").style.display = "hidden";
+      }
+      if (count < click) {
+        slideList.style.left = "-" + count * 31 + "rem";
+  
+        document.getElementById("leftSwipe").style.opacity = 1;
+        count++;
+      } else {
+        document.getElementById("rightSwipe").style.opacity = 0.5;
+        document.getElementById("rightSwipe").style.display = "hidden";
+      }
+    }
+    document
+      .getElementById("leftSwipe")
+      .addEventListener("click", stepBack);
+    document
+      .getElementById("rightSwipe")
+      .addEventListener("click", stepForward);
+  }
+ 
+
+
+
+//   function brandSlider1() {
+//     var slideList = document.querySelector(".slider");
+  
+//     var count = 1;
+//     var click = 8;
+//     var size = document.querySelector(".slider").offsetWidth;
+//     if (size < 900) {
+//       click = 11;
+//     }
+//     if (count == 1) {
+//       document.getElementById("leftSwipe1").style.opacity = 0.5;
+//       document.getElementById("leftSwipe1").style.display = "hidden";
+//     }
+//     console.log(size);
+//     function stepBack1() {
+//       if (count == 2) {
+//         document.getElementById("leftSwipe1").style.opacity = 0.5;
+//       }
+//       if (count > 1) {
+//         count = count - 2;
+//         slideList.style.left = "-" + count * 31 + "rem";
+  
+//         document.getElementById("rightSwipe1").style.opacity = 1;
+  
+//         count++;
+//       } else {
+//         document.getElementById("leftSwipe1").style.opacity = 0.5;
+//         document.getElementById("leftSwipe1").style.display = "hidden";
+//       }
+//     }
+//     function stepForward1() {
+//       if (count == click - 1) {
+//         document.getElementById("rightSwipe1").style.opacity = 0.5;
+//         document.getElementById("rightSwipe1").style.display = "hidden";
+//       }
+//       if (count < click) {
+//         slideList.style.left = "-" + count * 31 + "rem";
+  
+//         document.getElementById("leftSwipe1").style.opacity = 1;
+//         count++;
+//       } else {
+//         document.getElementById("rightSwipe1").style.opacity = 0.5;
+//         document.getElementById("rightSwipe1").style.display = "hidden";
+//       }
+//     }
+//     document
+//       .getElementById("leftSwipe1")
+//       .addEventListener("click", stepBack1);
+//     document
+//       .getElementById("rightSwipe1")
+//       .addEventListener("click", stepForward1);
+//   }
+  
+   
+ // brandSlider1();
+  brandSlider();
+
+
+
+// window.onload = function(){
+
+// }
+
+
+//last slider
+
+
+function brandSlider10() {
+    var slideList = document.querySelector(".slider_div");
+  
+    var count = 1;
+    var click = 8;
+    var size = document.querySelector(".slider_div").offsetWidth;
+    if (size < 900) {
+      click = 11;
+    }
+    if (count == 1) {
+      document.getElementById("leftSwipe10").style.opacity = 0.5;
+      document.getElementById("leftSwipe10").style.display = "hidden";
+    }
+    console.log(size);
+    function stepBack10() {
+      if (count == 2) {
+        document.getElementById("leftSwipe10").style.opacity = 0.5;
+      }
+      if (count > 1) {
+        count = count - 2;
+        slideList.style.left = "-" + count * 31 + "rem";
+  
+        document.getElementById("rightSwipe10").style.opacity = 1;
+  
+        count++;
+      } else {
+        document.getElementById("leftSwipe10").style.opacity = 0.5;
+        document.getElementById("leftSwipe10").style.display = "hidden";
+      }
+    }
+    function stepForward10() {
+      if (count == click - 1) {
+        document.getElementById("rightSwipe10").style.opacity = 0.5;
+        document.getElementById("rightSwipe10").style.display = "hidden";
+      }
+      if (count < click) {
+        slideList.style.left = "-" + count * 31 + "rem";
+  
+        document.getElementById("leftSwipe10").style.opacity = 1;
+        count++;
+      } else {
+        document.getElementById("rightSwipe10").style.opacity = 0.5;
+        document.getElementById("rightSwipe10").style.display = "hidden";
+      }
+    }
+    document
+      .getElementById("leftSwipe10")
+      .addEventListener("click", stepBack10);
+    document
+      .getElementById("rightSwipe10")
+      .addEventListener("click", stepForward10);
+  }
+  
+  brandSlider10();
